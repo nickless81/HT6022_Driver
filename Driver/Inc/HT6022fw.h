386a7ffde9f05ba14b65c29e64eee12ced56612b
Jose Pablo Castro Valverde <josepablo.castro@gmail.com>
2015-10-10 23:54:53 -0600

    Fix for: identation, includepath and include Firmware6022BX Class

--- Driver/Inc/HT6022fw.h
+++ Driver/Inc/HT6022fw.h
@@ -5,7 +5,7 @@
   * @email   rpm_2003_rpm@yahoo.com.br
   * @version V1.0
   * @date    04-14-2013
-  * @brief   This file provides the HT6022 firmware header         
+  * @brief   This file provides the HT6022 firmware header
   ******************************************************************************
   */
 
@@ -23,19 +23,19 @@
 /** @addtogroup HT6022_Driver
   * @{
   */
-/* Private define ------------------------------------------------------------*/ 
-#define HT6022_FIRMWARE_SIZE 458
-#define HT6022_FIRMWARE_VENDOR_ID    0X4B4
-#define HT6022_FIRMWARE_REQUEST_TYPE 0X40
-#define HT6022_FIRMWARE_REQUEST      0XA0
-#define HT6022_FIRMWARE_INDEX        0X00
+/* Private define ------------------------------------------------------------*/
+#define HT6022_FIRMWARE_SIZE            458
+#define HT6022_FIRMWARE_VENDOR_ID       0X4B4
+#define HT6022_FIRMWARE_REQUEST_TYPE    0X40
+#define HT6022_FIRMWARE_REQUEST         0XA0
+#define HT6022_FIRMWARE_INDEX           0X00
 
 /*Exported variables---------------------------------------------------------*/
 extern unsigned char HT6022_Firmware[];
 
 /**
   * @}
-  */ 
+  */
 
 #ifdef __cplusplus
     }
--- Hantek6022BX/ht6022bx/ht6022bx.pro
+++ Hantek6022BX/ht6022bx/ht6022bx.pro
@@ -10,11 +10,17 @@ TARGET = ht6022bx
 TEMPLATE = lib
 
 DEFINES += HT6022BX_LIBRARY
-
-SOURCES += ht6022bx.cpp
+INCLUDEPATH += ../../Driver/Inc/
+SOURCES += ht6022bx.cpp \
+    ../../Driver/Src/HT6022fw.c \
+    ../../Driver/Src/HT6022BLfw.c \
+    firmware6022bx.cpp
 
 HEADERS += ht6022bx.h\
-        ht6022bx_global.h
+        ht6022bx_global.h \
+    ../../Driver/Inc/HT6022fw.h \
+    ../../Driver/Inc/HT6022BLfw.h \
+    firmware6022bx.h
 
 unix {
     target.path = /usr/lib
