1918a3d4dc3132739e47e8b7baac93f9038fbcae
Jose Pablo Castro Valverde <josepablo.castro@gmail.com>
2015-10-10 19:36:56 -0600

    Adding Firmware for Hantek6022BL

--- Driver/Inc/HT6022BLfw.h
+++ Driver/Inc/HT6022BLfw.h
@@ -24,7 +24,7 @@
   * @{
   */
 /* Private define ------------------------------------------------------------*/ 
-#define HT6022BL_FIRMWARE_SIZE          470
+#define HT6022BL_FIRMWARE_SIZE          472
 #define HT6022BL_FIRMWARE_VENDOR_ID     0X04B4
 #define HT6022BL_FIRMWARE_PRODUCT_ID    0X602A
 #define HT6022BL_FIRMWARE_REQUEST_TYPE  0X40
